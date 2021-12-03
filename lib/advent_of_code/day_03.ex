d6fb30533ba794d91d1932a82b00b3bf73c95a7b
Marcus Cemes <marcus.cemes@gmail.com>
2021-12-03 13:51:33 +0100

    perf: improve day 03

--- lib/advent_of_code/day_03.ex
+++ lib/advent_of_code/day_03.ex
@@ -3,43 +3,16 @@ defmodule AdventOfCode.Day03 do
 
   @spec part1(Stream.t(binary)) :: integer
   def part1(args) do
-    most_frequent =
-      parse_args(args)
-      |> Enum.to_list()
-      |> transpose()
-      |> Enum.map(&most_frequent/1)
-
-    gamma =
-      most_frequent
-      |> Integer.undigits(2)
-
-    epsilon =
-      most_frequent
-      |> Enum.map(&flip_bit/1)
-      |> Integer.undigits(2)
-
+    frequencies = args |> parse_args() |> dominant_bits()
+    gamma = frequencies |> Integer.undigits(2)
+    epsilon = frequencies |> Enum.map(&Bitwise.bxor(&1, 1)) |> Integer.undigits(2)
     gamma * epsilon
-  end
-
-  @spec transpose([[integer]]) :: [[integer]]
-  defp transpose(mx) do
-    mx |> List.zip() |> Enum.map(&Tuple.to_list/1)
-  end
-
-  @spec most_frequent([integer]) :: integer
-  defp most_frequent(items) do
-    items
-    |> Enum.frequencies()
-    |> Enum.max_by(&elem(&1, 1))
-    |> elem(0)
   end
 
   def part2(args) do
     data = parse_args(args) |> Enum.to_list()
-
-    oxygen_rating = filter_rating_by(data, :most)
-    c02_rating = filter_rating_by(data, :least)
-
+    oxygen_rating = filter_rating_by(data, :oxygen)
+    c02_rating = filter_rating_by(data, :c02)
     oxygen_rating * c02_rating
   end
 
@@ -56,21 +29,15 @@ defmodule AdventOfCode.Day03 do
   end
 
   defp filter_rating_by(entries, type, position) when length(entries) > 1 do
-    {zeros, ones} =
+    selected_bit =
       entries
-      |> Enum.reduce({0, 0}, fn entry, {zeros, ones} ->
-        case Enum.at(entry, position) do
-          0 -> {zeros + 1, ones}
-          1 -> {zeros, ones + 1}
-        end
-      end)
-
-    most_frequent = if ones >= zeros, do: 1, else: 0
+      |> dominant_bits()
+      |> Enum.at(position)
 
     required_bit =
       case type do
-        :most -> most_frequent
-        :least -> flip_bit(most_frequent)
+        :oxygen -> selected_bit
+        :c02 -> Bitwise.bxor(selected_bit, 1)
       end
 
     entries
@@ -78,13 +45,28 @@ defmodule AdventOfCode.Day03 do
     |> filter_rating_by(type, position + 1)
   end
 
-  @spec flip_bit(integer) :: integer
-  defp flip_bit(number) do
-    case number do
-      1 -> 0
-      0 -> 1
-    end
+  @spec dominant_bits(Stream.t([integer])) :: [integer]
+  defp dominant_bits(entries) do
+    [first_element] = entries |> Enum.take(1)
+    init = List.duplicate(0, length(first_element))
+
+    {number_ones, total} =
+      entries
+      |> Enum.reduce({init, 0}, fn entry, {count, total} ->
+        new_freq =
+          Enum.zip(count, entry)
+          |> Enum.map(fn {a, b} -> a + b end)
+
+        {new_freq, total + 1}
+      end)
+
+    threshold = total / 2
+    Enum.map(number_ones, &bool_to_int(&1 >= threshold))
   end
+
+  @spec bool_to_int(boolean) :: 0 | 1
+  def bool_to_int(true), do: 1
+  def bool_to_int(false), do: 0
 
   @spec parse_args(Stream.t(binary)) :: Stream.t([integer])
   defp parse_args(args) do
--- mix.exs
+++ mix.exs
@@ -21,7 +21,7 @@ defmodule AdventOfCode.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:benchee, "~> 1.0"},
+      {:benchee, "~> 1.0"}
     ]
   end
 end
