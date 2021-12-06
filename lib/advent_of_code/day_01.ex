fe5a1e26aaf9dc5a1827690e2a36438dec950f11
Marcus Cemes <marcus.cemes@gmail.com>
2021-12-06 20:49:21 +0100

    style: improve day 1

--- lib/advent_of_code/day_01.ex
+++ lib/advent_of_code/day_01.ex
@@ -1,26 +1,26 @@
 defmodule AdventOfCode.Day01 do
   import AdventOfCode.Utils
 
-  @spec part1(Stream.t(binary)) :: integer
+  @spec part1(Stream.t(binary())) :: integer()
   def part1(args) do
     parse_args(args)
     |> Stream.chunk_every(2, 1, :discard)
     |> Enum.reduce(0, fn [p, c], acc -> if c > p, do: acc + 1, else: acc end)
   end
 
-  @spec part2(Stream.t(binary)) :: integer
+  @spec part2(Stream.t(binary())) :: integer()
   def part2(args) do
     parse_args(args)
     |> Stream.chunk_every(3, 1, :discard)
     |> Stream.map(fn [a, b, c] -> a + b + c end)
     |> Stream.chunk_every(2, 1, :discard)
-    |> Enum.reduce(0, fn [p, i], acc -> if i > p, do: acc + 1, else: acc end)
+    |> Enum.reduce(0, fn [p, c], acc -> if c > p, do: acc + 1, else: acc end)
   end
 
-  @spec parse_args(Stream.t(binary)) :: [integer]
-  def parse_args(args) do
+  @spec parse_args(Stream.t(binary())) :: Stream.t(integer())
+  defp parse_args(args) do
     args
     |> sanitise_stream()
-    |> Enum.map(&parse_int/1)
+    |> Stream.map(&parse_int/1)
   end
 end
