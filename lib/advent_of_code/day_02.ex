4acda0b0171e81ba6e66b34f7c3b646af49437eb
Marcus Cemes <marcus.cemes@gmail.com>
2021-12-06 21:39:55 +0100

    chore: improve day 2

--- lib/advent_of_code/day_02.ex
+++ lib/advent_of_code/day_02.ex
@@ -1,46 +1,50 @@
 defmodule AdventOfCode.Day02 do
-  @spec part1(Stream.t(binary)) :: integer
+  import AdventOfCode.Utils
+
+  @type simple_state :: {integer(), integer()}
+  @type advanced_state :: {integer(), integer(), integer()}
+
+  @spec part1(Stream.t(binary())) :: integer()
   def part1(args) do
-    {distance, depth} =
-      parse_args(args)
-      |> Enum.reduce({0, 0}, fn instruction, {distance, depth} ->
-        case instruction do
-          {"forward", delta} -> {distance + delta, depth}
-          {"down", delta} -> {distance, depth + delta}
-          {"up", delta} -> {distance, depth - delta}
-        end
-      end)
-
-    distance * depth
+    parse_args(args)
+    |> Enum.reduce({0, 0}, &simple_reducer/2)
+    |> Kernel.then(fn {a, b} -> a * b end)
   end
 
-  @spec part2(Stream.t(binary)) :: integer
+  @spec simple_reducer({String.t(), integer()}, simple_state()) :: simple_state()
+  defp simple_reducer({instruction, count}, {distance, depth}) do
+    case instruction do
+      "forward" -> {distance + count, depth}
+      "down" -> {distance, depth + count}
+      "up" -> {distance, depth - count}
+    end
+  end
+
+  @spec part2(Stream.t(binary())) :: integer()
   def part2(args) do
-    {distance, depth, _} =
-      parse_args(args)
-      |> Enum.reduce({0, 0, 0}, fn instruction, {distance, depth, aim} ->
-        case instruction do
-          {"forward", delta} -> {distance + delta, depth + aim * delta, aim}
-          {"down", delta} -> {distance, depth, aim + delta}
-          {"up", delta} -> {distance, depth, aim - delta}
-        end
-      end)
-
-    distance * depth
+    parse_args(args)
+    |> Enum.reduce({0, 0, 0}, &advanced_reducer/2)
+    |> Kernel.then(fn {distance, depth, _} -> distance * depth end)
   end
 
-  @spec parse_args(Stream.t(binary)) :: Stream.t({String.t(), integer})
+  @spec advanced_reducer({String.t(), integer()}, advanced_state()) :: advanced_state()
+  defp advanced_reducer({instruction, count}, {distance, depth, aim}) do
+    case instruction do
+      "forward" -> {distance + count, depth + aim * count, aim}
+      "down" -> {distance, depth, aim + count}
+      "up" -> {distance, depth, aim - count}
+    end
+  end
+
+  @spec parse_args(Stream.t(binary())) :: Stream.t({String.t(), integer()})
   def parse_args(args) do
-    args
+    sanitise_stream(args)
     |> Stream.map(&parse_instruction/1)
   end
 
-  @spec parse_instruction(binary) :: {String.t(), integer}
+  @spec parse_instruction(binary()) :: {String.t(), integer()}
   def parse_instruction(line) do
-    [instruction, paramter] = String.split(line, " ")
-
-    case Integer.parse(paramter) do
-      {number, _} -> {instruction, number}
-    end
+    [instruction, count] = String.split(line, " ")
+    {instruction, parse_int(count)}
   end
 end
