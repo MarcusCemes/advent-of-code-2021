12cf69feb086ab26ed2037c4d0f637599e66b770
Marcus Cemes <marcus.cemes@gmail.com>
2021-12-08 13:15:45 +0100

    style: improve day 8

--- lib/advent_of_code/day_08.ex
+++ lib/advent_of_code/day_08.ex
@@ -15,61 +15,61 @@ defmodule AdventOfCode.Day08 do
   @spec part2(Stream.t(binary())) :: integer()
   def part2(args) do
     parse_args(args)
-    |> Stream.map(&decode_line/1)
-    |> Stream.map(&join_digits/1)
+    |> Stream.map(&decode_display/1)
     |> Enum.sum()
   end
 
-  # Decodes the scrambled line, returning a list of the correctly decoded digits
-  @spec decode_line(line()) :: [integer()]
-  defp decode_line({signals, values}) do
-    values
-    |> Enum.map(&correct_signal(&1, correction_map(signals)))
+  # Solves the misconfiguration and returns the corrected displayed number
+  @spec decode_display(line()) :: integer()
+  defp decode_display({test_signals, displayed_digits}) do
+    displayed_digits
+    |> Enum.map(&correct_display(&1, segment_correction_map(test_signals)))
     |> Enum.map(&decode/1)
+    |> Integer.undigits()
   end
 
-  # Given an array of digits, joins them into a base-10 number
-  @spec join_digits([integer()]) :: integer()
-  defp join_digits(digits), do: Enum.reduce(digits, 0, &(10 * &2 + &1))
+  # Generate a map that takes an (falsely) illuminated segment, giving
+  # the segment that should be correctly illuminated, by analysing
+  # the segment illumination frequencies during the test signals
+  @spec segment_correction_map([String.t()]) :: mapping()
+  defp segment_correction_map(test_signals) do
+    segment_graphemes = Enum.flat_map(test_signals, &String.graphemes/1)
 
-  # Calculate the correction map based on the segment illumination
-  # frequencies during the ten test signals
-  @spec correction_map([String.t()]) :: mapping()
-  defp correction_map(signals) do
-    segment_graphemes = Enum.flat_map(signals, &String.graphemes/1)
+    four = Enum.find(test_signals, &(String.length(&1) == 4))
+    seven = Enum.find(test_signals, &(String.length(&1) == 3))
 
-    four = Enum.find(signals, &(String.length(&1) == 4))
-    seven = Enum.find(signals, &(String.length(&1) == 3))
+    # By adding two fours and two sevens, the illumination frequencies
+    # during the test signals become independent from the rest.
+    # The correct segment letter can then be mapped to the falsely illuminated segment.
+    frequency_offset = [four, four, seven, seven] |> Enum.flat_map(&String.graphemes/1)
+    independent_frequencies = Enum.frequencies(frequency_offset ++ segment_graphemes)
 
-    # By adding two fours and two sevens, the frequency of each segment
-    # is made independent from the rest, the offset must be taken into
-    # account in ind_lin_freq_to_segment()
-    lin_ind_offset = [four, four, seven, seven] |> Enum.flat_map(&String.graphemes/1)
-    lin_ind_frequencies = Enum.frequencies(lin_ind_offset ++ segment_graphemes)
-
-    lin_ind_frequencies
+    independent_frequencies
     |> Map.to_list()
-    |> Enum.map(fn {char, frequency} -> {char, ind_lin_freq_to_segment(frequency)} end)
+    |> Enum.map(fn {char, frequency} -> {char, frequency_to_segment(frequency)} end)
     |> Map.new()
   end
 
-  # Maps the linearly independent frequencies to the correct segment letter
-  @spec ind_lin_freq_to_segment(4 | 7 | 8 | 9 | 10 | 12 | 13) :: String.t()
-  defp ind_lin_freq_to_segment(10), do: "a"
-  defp ind_lin_freq_to_segment(8), do: "b"
-  defp ind_lin_freq_to_segment(12), do: "c"
-  defp ind_lin_freq_to_segment(9), do: "d"
-  defp ind_lin_freq_to_segment(4), do: "e"
-  defp ind_lin_freq_to_segment(13), do: "f"
-  defp ind_lin_freq_to_segment(7), do: "g"
+  # Maps the offset independent frequencies to the correct segment letter
+  @spec frequency_to_segment(integer()) :: String.t()
+  defp frequency_to_segment(frequency) do
+    case frequency do
+      10 -> "a"
+      8 -> "b"
+      12 -> "c"
+      9 -> "d"
+      4 -> "e"
+      13 -> "f"
+      7 -> "g"
+    end
+  end
 
   # Applies the correction map to the signal and returns an
   # alphabetically sorted string of illuminated segments
-  @spec correct_signal(String.t(), mapping()) :: String.t()
-  defp correct_signal(signal, correction) do
-    signal
-    |> String.graphemes()
-    |> Enum.map(&Map.get(correction, &1))
+  @spec correct_display(String.t(), mapping()) :: String.t()
+  defp correct_display(segments, correction_map) do
+    String.graphemes(segments)
+    |> Enum.map(&Map.get(correction_map, &1))
     |> Enum.sort()
     |> Enum.join()
   end
@@ -77,16 +77,20 @@ defmodule AdventOfCode.Day08 do
   # Returns the numerical number that corresponds to a given
   # set of illuminated segments (in alphabetical order)
   @spec decode(String.t()) :: integer()
-  defp decode("abcefg"), do: 0
-  defp decode("cf"), do: 1
-  defp decode("acdeg"), do: 2
-  defp decode("acdfg"), do: 3
-  defp decode("bcdf"), do: 4
-  defp decode("abdfg"), do: 5
-  defp decode("abdefg"), do: 6
-  defp decode("acf"), do: 7
-  defp decode("abcdefg"), do: 8
-  defp decode("abcdfg"), do: 9
+  defp decode(segments) do
+    case segments do
+      "abcefg" -> 0
+      "cf" -> 1
+      "acdeg" -> 2
+      "acdfg" -> 3
+      "bcdf" -> 4
+      "abdfg" -> 5
+      "abdefg" -> 6
+      "acf" -> 7
+      "abcdefg" -> 8
+      "abcdfg" -> 9
+    end
+  end
 
   @spec parse_args(Stream.t(binary())) :: Stream.t(line())
   defp parse_args(args) do
