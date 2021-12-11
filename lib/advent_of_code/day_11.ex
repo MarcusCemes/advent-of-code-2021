7be405becc22dd6a13d22e1f2d9cb7068f87ef23
Marcus Cemes <marcus.cemes@gmail.com>
2021-12-11 23:18:25 +0100

    feat: improve day 11 - Redesigned solution to guarantee tail recursion optimisation - Improved performance

--- lib/advent_of_code/day_11.ex
+++ lib/advent_of_code/day_11.ex
@@ -1,39 +1,34 @@
 defmodule AdventOfCode.Day11 do
   import AdventOfCode.Utils
 
-  @typep charge_map() :: [[integer()]]
+  @typep int_map() :: [[integer()]]
   @typep coordinates() :: {integer(), integer()}
+  @typep map_with_coords :: {int_map(), [coordinates()]}
 
   @spec part1(Stream.t(binary)) :: integer()
   @spec part2(Stream.t(binary)) :: integer()
-  def part1(args), do: parse_args(args) |> simulate_flashes(100)
+  def part1(args), do: parse_args(args) |> count_flashes(100) |> elem(1)
   def part2(args), do: parse_args(args) |> until_synchronised()
 
-  @spec simulate_flashes(charge_map(), integer(), integer()) :: integer()
-  defp simulate_flashes(map, remaining, flashes \\ 0)
-  defp simulate_flashes(_, 0, flashes), do: flashes
+  @spec count_flashes(int_map(), integer()) :: {map, integer()}
+  @spec count_flash_reducer(integer(), {int_map(), integer()}) :: {int_map(), integer()}
+  defp count_flashes(map, n_steps), do: Enum.reduce(1..n_steps, {map, 0}, &count_flash_reducer/2)
+  defp count_flash_reducer(_, {map, acc_flash}), do: next_step(map, acc_flash)
 
-  defp simulate_flashes(map, remaining, flashes) do
-    {new_flashes, map} = next_step(map)
-    simulate_flashes(map, remaining - 1, flashes + Enum.sum(new_flashes))
+  @spec until_synchronised(int_map()) :: integer()
+  defp until_synchronised(map, current_step \\ 0) do
+    if synchronised?(map),
+      do: current_step,
+      else: until_synchronised(next_step(map) |> elem(0), current_step + 1)
   end
 
-  @spec until_synchronised(charge_map()) :: integer()
-  defp until_synchronised(map, step \\ 0) do
-    if synchronised(map),
-      do: step,
-      else: until_synchronised(next_step(map) |> elem(1), step + 1)
-  end
-
-  @spec next_step(charge_map()) :: {[integer()], charge_map()}
-  defp next_step(map) do
-    {map, charged} = increment_charge(map)
-    Enum.map_reduce(charged, map, &flash/2)
-  end
+  # Carry out a step of the simulation
+  @spec next_step(int_map(), integer()) :: {int_map(), integer()}
+  defp next_step(map, acc_flash \\ 0), do: increment_charge(map) |> propagate_flashes(acc_flash)
 
   # Octopuses are all incremented during a single sweep instead of individual
   # updates to avoid unnecessary memory allocation due to immutability
-  @spec increment_charge(charge_map()) :: {charge_map(), [coordinates()]}
+  @spec increment_charge(int_map()) :: map_with_coords()
   defp increment_charge(map) do
     Enum.map_reduce(Enum.with_index(map), [], fn {line, y}, charged ->
       Enum.map_reduce(Enum.with_index(line), charged, fn {item, x}, charged ->
@@ -42,43 +37,45 @@ defmodule AdventOfCode.Day11 do
     end)
   end
 
-  # flash/2 and charge/2 are not optimised for tail recursion for sake of complexity.
-  # The stack size will likely be the limiting factor for the size of the map.
+  # Carries out cascade flash propagation using tail-end recursion
+  @spec propagate_flashes(map_with_coords(), integer()) :: {int_map(), integer()}
+  defp propagate_flashes({map, []}, acc_flashes), do: {map, acc_flashes}
 
-  @spec flash(coordinates(), charge_map()) :: {integer(), charge_map()}
-  defp flash(coords, map) do
-    {flashes, map} = Enum.map_reduce(adjacent(coords), set(coords, map, 0), &charge/2)
-    {Enum.sum(flashes) + 1, map}
+  defp propagate_flashes({map, region}, acc_flashes) do
+    Enum.flat_map(region, &area_coordinates/1)
+    |> Enum.reduce({map, []}, &charge_octopus/2)
+    |> propagate_flashes(acc_flashes + length(region))
   end
 
-  @spec charge(coordinates(), charge_map()) :: {integer(), charge_map()}
-  defp charge(coords, map) do
-    case at(coords, map) do
-      x when x == 0 or x == nil -> {0, map}
-      charge when charge < 9 -> {0, set(coords, map, charge + 1)}
-      _ -> flash(coords, map)
+  # An octopus can be charged at charge-levels 1-9 and flashes when it reaches level 10
+  @spec charge_octopus(coordinates(), map_with_coords()) :: map_with_coords()
+  defp charge_octopus(coords, {map, charged}) do
+    case get_at(coords, map) do
+      charge when charge == 0 or charge == nil -> {map, charged}
+      charge when charge >= 9 -> {set(coords, map, 0), [coords | charged]}
+      charge -> {set(coords, map, charge + 1), charged}
     end
   end
 
-  @spec adjacent(coordinates()) :: [coordinates()]
-  defp adjacent({x, y}),
-    do: for(dx <- -1..1, dy <- -1..1, dx != 0 or dy != 0, do: {x + dx, y + dy})
+  # == Utilities == #
 
-  @spec at(coordinates(), charge_map()) :: integer() | nil
-  defp at({x, y}, _) when x < 0 or y < 0, do: nil
-  defp at({x, y}, map), do: Enum.at(map, y, []) |> Enum.at(x)
+  @spec area_coordinates(coordinates()) :: [coordinates()]
+  defp area_coordinates({x, y}), do: for(dx <- -1..1, dy <- -1..1, do: {x + dx, y + dy})
 
-  @spec set(coordinates(), charge_map(), integer()) :: charge_map()
-  defp set({x, y}, map, value),
-    do: List.update_at(map, y, &List.replace_at(&1, x, value))
+  @spec get_at(coordinates(), int_map()) :: integer() | nil
+  defp get_at({x, y}, _) when x < 0 or y < 0, do: nil
+  defp get_at({x, y}, map), do: Enum.at(map, y, []) |> Enum.at(x)
 
-  @spec synchronised(charge_map()) :: boolean()
-  defp synchronised(map), do: length(Enum.flat_map(map, & &1) |> Enum.uniq()) == 1
+  @spec set(coordinates(), int_map(), integer()) :: int_map()
+  defp set({x, y}, map, val), do: List.update_at(map, y, &List.replace_at(&1, x, val))
+
+  @spec synchronised?(int_map()) :: boolean()
+  defp synchronised?(map) do
+    target = map |> hd() |> hd()
+    Enum.all?(map, fn line -> Enum.all?(line, &(&1 == target)) end)
+  end
 
   @spec parse_args(Stream.t(binary)) :: [[integer()]]
-  defp parse_args(args) do
-    Enum.map(args, fn line ->
-      String.trim(line) |> String.graphemes() |> Enum.map(&parse_int/1)
-    end)
-  end
+  defp parse_args(args), do: Enum.map(args, &parse_line/1)
+  defp parse_line(line), do: String.trim(line) |> String.graphemes() |> Enum.map(&parse_int/1)
 end
