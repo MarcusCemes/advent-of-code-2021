dfc86f8fea63ecad3be86d334de4b132aa2b5e1d
Marcus Cemes <marcus.cemes@gmail.com>
2021-12-17 11:54:33 +0100

    perf: improve day 15

--- lib/advent_of_code/day_15.ex
+++ lib/advent_of_code/day_15.ex
@@ -2,32 +2,33 @@ defmodule AdventOfCode.Day15 do
   import AdventOfCode.Utils
 
   @typep matrix :: [[integer]]
+  @typep baked_matrix :: tuple
   @typep coords :: {integer, integer}
   @typep priority_queue :: [{coords, integer}]
   @typep visit_map :: Map.t(coords, integer)
 
   @spec part1([binary]) :: integer
   def part1(args) do
-    cavern = parse_args(args)
-    dimensions = matrix_dimensions(cavern)
+    cavern = parse_args(args) |> bake_matrix()
+    dimensions = baked_matrix_dimensions(cavern)
     path_find(cavern, dimensions, {0, 0})
   end
 
   @spec part2([binary]) :: integer
   def part2(args) do
-    cavern = parse_args(args) |> tile_cavern()
-    dimensions = matrix_dimensions(cavern)
+    cavern = parse_args(args) |> tile_matrix() |> bake_matrix()
+    dimensions = baked_matrix_dimensions(cavern)
     path_find(cavern, dimensions, {0, 0})
   end
 
-  @spec path_find(matrix, coords, coords) :: integer
+  @spec path_find(baked_matrix, coords, coords) :: integer
   defp path_find(cavern, target, start) do
     do_path_find(cavern, target, [{start, 0}], Map.new())
   end
 
   # This is the hot loop, it will recursively pick a coordinate from the
   # head of the priority queue and run a variant of the Dijkstra algorithm.
-  @spec do_path_find(matrix, coords, priority_queue, visit_map) :: integer
+  @spec do_path_find(baked_matrix, coords, priority_queue, visit_map) :: integer
   defp do_path_find(_, target, [{target, risk} | _], _), do: risk
 
   defp do_path_find(cavern, target, [{coords, risk} | queue], visited) do
@@ -40,10 +41,10 @@ defmodule AdventOfCode.Day15 do
     end
   end
 
-  @spec extend_queue(matrix, coords, integer, priority_queue) :: priority_queue
+  @spec extend_queue(baked_matrix, coords, integer, priority_queue) :: priority_queue
   defp extend_queue(cavern, coords, risk, queue) do
     adjacent_coordinates(coords)
-    |> Enum.map(fn pos -> {pos, matrix_at(pos, cavern)} end)
+    |> Enum.map(fn pos -> {pos, cavern_at(pos, cavern)} end)
     |> Enum.filter(fn {_, risk} -> risk != nil end)
     |> Enum.map(fn {pos, pos_risk} -> {pos, risk + pos_risk} end)
     |> insert_queue(queue)
@@ -62,27 +63,26 @@ defmodule AdventOfCode.Day15 do
   defp do_insert_queue([], queue), do: queue
 
   defp do_insert_queue(sorted_items, queue) do
-    risk = hd(sorted_items) |> elem(1)
-    queue_risk = hd(queue) |> elem(1)
+    {_, risk} = hd(sorted_items)
+    {_, queue_risk} = hd(queue)
 
     if risk < queue_risk do
-      [head | tail] = sorted_items
-      [head | do_insert_queue(tail, queue)]
+      [hd(sorted_items) | do_insert_queue(tl(sorted_items), queue)]
     else
       [hd(queue) | do_insert_queue(sorted_items, tl(queue))]
     end
   end
 
   # Takes a matrix and tiles it into a 5x5 with the correct increments (see problem)
-  @spec tile_cavern(matrix) :: matrix
-  defp tile_cavern(cavern) do
+  @spec tile_matrix(matrix) :: matrix
+  defp tile_matrix(matrix) do
     tile_offsets()
-    |> matrix_map(&cavern_add(cavern, &1))
+    |> matrix_map(&matrix_add(matrix, &1))
     |> join_matrices()
   end
 
-  @spec cavern_add(matrix, integer) :: matrix
-  defp cavern_add(cavern, count), do: matrix_map(cavern, &cell_add(&1, count))
+  @spec matrix_add(matrix, integer) :: matrix
+  defp matrix_add(matrix, count), do: matrix_map(matrix, &cell_add(&1, count))
   defp cell_add(value, count), do: rem(value + count - 1, 9) + 1
 
   # Generates the matrix of offsets that need to be added to each tile
@@ -99,24 +99,35 @@ defmodule AdventOfCode.Day15 do
 
   # == Utilities == #
 
-  @spec matrix_at(coords, Map.t(coords, t)) :: t | nil when t: var
-  defp matrix_at({x, y}, _) when x == -1 or y == -1, do: nil
-  defp matrix_at({x, y}, cavern), do: cavern |> Enum.at(y, []) |> Enum.at(x)
+  @spec cavern_at(coords, baked_matrix) :: integer | nil
+  defp cavern_at({x, y}, cavern) do
+    try do
+      cavern |> elem(y) |> elem(x)
+    rescue
+      # The indices may be out of bounds
+      ArgumentError -> nil
+    end
+  end
 
-  @spec matrix_map([[t]], (t -> u)) :: [[u]] when t: var, u: var
-  defp matrix_map(map, map_fn) do
-    Enum.map(map, fn line -> Enum.map(line, &map_fn.(&1)) end)
+  @spec matrix_map(matrix, (integer -> integer)) :: matrix
+  defp matrix_map(matrix, map_fn) do
+    Enum.map(matrix, fn line -> Enum.map(line, &map_fn.(&1)) end)
   end
 
   @spec adjacent_coordinates(coords) :: [coords]
   defp adjacent_coordinates({x, y}), do: [{x + 1, y}, {x, y + 1}, {x - 1, y}, {x, y - 1}]
 
-  @spec matrix_dimensions(matrix) :: coords
-  defp matrix_dimensions(matrix) do
-    y_size = length(matrix) - 1
-    x_size = (hd(matrix) |> length()) - 1
+  @spec baked_matrix_dimensions(baked_matrix) :: {integer, integer}
+  defp baked_matrix_dimensions(matrix) do
+    y_size = tuple_size(matrix) - 1
+    x_size = (elem(matrix, 0) |> tuple_size()) - 1
     {x_size, y_size}
   end
+
+  # Converts a two-dimensional list to a two-dimensional tuple.
+  # Tuples are good for read-only items, as they provide O(1) access.
+  @spec bake_matrix(matrix) :: baked_matrix
+  defp bake_matrix(matrix), do: Enum.map(matrix, &List.to_tuple/1) |> List.to_tuple()
 
   @spec parse_args([binary]) :: matrix
   defp parse_args(args), do: Enum.map(args, &parse_line/1)
